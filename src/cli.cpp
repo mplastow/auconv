a47d856a6ce9cdc8e93eacd84cae94e4b40294f8
Matt Plastow <dfulcrum@gmail.com>
2025-08-01 17:29:00 -0400

    Add code to parse more cli args

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,5 +1,6 @@
 // cli.cpp - auconv
 
+#include <algorithm>
 #include <cstdlib>
 #include <filesystem>
 #include <iostream>
@@ -59,7 +60,12 @@ namespace {
         case PathType::Invalid: {
             std::cout << "Unspecified conversion mode. Use auconv --help" << '\n';
             std::quick_exit(1);
-        } break;
+        } // break;
+
+        default: {
+            std::cout << "You found some unreachable code!\n";
+            std::unreachable();
+        } // break;
         }
 
         switch (got) {
@@ -90,11 +96,46 @@ namespace {
         return path_type;
     }
 
+    FormatTag getFormat(std::string_view sv)
+    {
+        if (sv == "wav") {
+            return FormatTag::wav;
+        } else if (sv == "fla") {
+            return FormatTag::flac;
+        } else if (sv == "mp3") {
+            return FormatTag::mp3;
+        } else if (sv == "all") {
+            return FormatTag::all;
+        } else {
+            return FormatTag::null;
+        }
+    }
+
+    FormatTag parseFormatArg(std::string_view sv)
+    {
+        if (sv.starts_with("-from=")) {
+            auto fmt = sv.substr(6, 3);
+            std::cout << fmt << '\n';
+            return getFormat(fmt);
+        } else if (sv.starts_with("-to=")) {
+            auto fmt = sv.substr(4, 3);
+            std::cout << fmt << '\n';
+            return getFormat(fmt);
+        } else {
+            return FormatTag::null;
+        }
+    }
+
+    void printErrorArgNumber()
+    {
+        std::cout << "Incorrect number of arguments. Use auconv --help" << '\n';
+        std::quick_exit(1);
+    }
+
     CLIArgs parseArgs(ArgArray const& args)
     {
-        Path path {args[2]};
+        Path path {args.at(2)};
 
-        /// TODO: (MATT) Test whether this is a redundant operation
         if (!std::filesystem::exists(path)) {
             std::cout << "The provided path is not valid." << '\n';
             std::quick_exit(1);
@@ -102,10 +143,36 @@ namespace {
 
         CLIArgs parsed_args {.path {path}, .mode {}};
 
-        std::string_view mode {args[1]};
+        std::string_view mode {args.at(1)};
         parsed_args.mode = parseMode(path, mode);
 
+        std::cout << "Parsing to/from\n";
+
         /// TODO: (MATT)  Parse args for input and output types
+        InFormat  from_fmt {FormatTag::all};
+        OutFormat to_fmt {FormatTag::null};
+        if (args.size() >= 4) {
+            std::cout << "n args: " << args.size() << "\n";
+            if (args.at(3).starts_with("-from=")) {
+                from_fmt.fmt_ = parseFormatArg(args.at(3));
+            } else if (args.at(3).starts_with("-to=")) {
+                to_fmt.fmt_ = parseFormatArg(args.at(3));
+            }
+        }
+
+        if (args.size() >= 5) {
+            std::cout << "n args: " << args.size() << "\n";
+            if (args.at(4).starts_with("-from=")) {
+                from_fmt.fmt_ = parseFormatArg(args.at(4));
+            } else if (args.at(4).starts_with("-to=")) {
+                to_fmt.fmt_ = parseFormatArg(args.at(4));
+            }
+        }
+
+        if (args.size() > 5) {
+            std::cout << "n args: " << args.size() << "\n";
+            printErrorArgNumber();
+        }
 
         return parsed_args;
     }
@@ -120,12 +187,6 @@ namespace {
         std::cout << "END OF CLI ARGS\n\n";
     }
 
-    void printErrorArgNumber()
-    {
-        std::cout << "Incorrect number of arguments. Use auconv --help" << '\n';
-        std::quick_exit(1);
-    }
-
     void printHelp()
     {
         std::cout << "auconv v0.01: a basic WAV to FLAC converter" << '\n';
@@ -136,6 +197,8 @@ namespace {
         std::cout << "\t-t: convert all files in a directory and all its subdirectories. [path] is path to top directory." << '\n';
         std::cout << "Path: must be absolute path, e.g. /home/user/audio/wav/, or '.' for current directory\n"
                   << '\n';
+        std::cout << "Input format may optionally be specified with '-from=<fmt>'." << '\n';
+        std::cout << "Output format must be specified with '-to=<fmt>'." << '\n';
 
         std::quick_exit(0);
     }
@@ -155,9 +218,11 @@ CLIArgs handleCLIArguments(ArgArray const& args)
             printErrorArgNumber();
         }
     } break;
-    case 3: {
+    case 3:
+    case 4:
+    case 5: {
         return parseArgs(args);
-    } break;
+    } // break;
 
     default: {
         printErrorArgNumber();
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 // main.cpp - auconv
 
+#include <array>
 #include <cstdio>
 
 #include <cli.hpp>
 #include <defines.hpp>
-#include <format.hpp>
 #include <snd.hpp>
 #include <types.hpp>
 
@@ -14,11 +14,6 @@ auto main(int argc, char* argv[]) -> int
     for (int i {0}; i < argc; ++i) {
         arg_vec.emplace_back(argv[i]);
     }
-
-    auconv::Format wav {1};
-    auconv::Format flac {2};
-
-    auconv::convert(wav, flac);
 
     auto parsed_args = auconv::handleCLIArguments(arg_vec);
 
